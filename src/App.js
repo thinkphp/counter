9348781236beb1feaf5d3e71d124547afce1fb4c
Adrian Statescu <mergesortv@gmail.com>
2024-08-23 19:05:49 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ColorPicker from './ColorPicker.js'
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      
+      <ColorPicker />
     </div>
   );
 }
---
+++ src/ColorPicker.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function ColorPicker() {
+  const [color, setColor] = useState('#000000');
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Color Picker</h1>
+      <input type="color" value={color} onChange={handleColorChange} />
+      <div style={{ backgroundColor: color,  width: '200px', height:  '100px'}}>
+        <p>Selected Color: {color}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ColorPicker;
